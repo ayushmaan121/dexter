90ea94dd36682ab1548b8f5dae25035cead3d02a
ayushmaan <ayushmaan_saxena@optum.com>
2020-10-22 12:08:25 +0530

    initial setup

---
+++ dexterAPI/Controllers/ActionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dexterAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dexterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActionController : Controller
+    {
+        private IAction _action;
+        public ActionController(IAction action)
+        {
+            _action = action;
+        }
+
+        [HttpGet]
+        public IActionResult GenerateZip()
+        {
+            return _action.GenerateZip();
+        }
+    }
+}
---
+++ dexterAPI/Services/Action.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dexterAPI.Services
+{
+    public class Action: IAction
+    {
+        public JsonResult GenerateZip()
+        {
+            return new JsonResult("hi");
+        }
+    }
+}
